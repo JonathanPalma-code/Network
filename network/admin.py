e0cab03f544e7452afc150b3e79eb03b13bc7649
Jonathan <jonathanpalma89@gmail.com>
2021-02-09 18:35:51 +0000

    chore: add super user

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import User, Profile, Follower, Following, Post, Comment, Like 
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'location', 'birth_date', 'photo')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'timestamp', 'orignal_poster')
+
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Follower)
+admin.site.register(Following)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
